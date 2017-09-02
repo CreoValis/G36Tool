18284ffc78238f0b02fbc0996f8d1bcd649bc2a2
Tallódi Mihály <msi1985@gmail.com>
2017-09-02 10:23:02 +0200

    G36Tool.cpp: serial port support

--- G36Tool/G36Tool.cpp
+++ G36Tool/G36Tool.cpp
@@ -1,10 +1,61 @@
 #include <iostream>
 #include <iomanip>
 
-#include <fstream>
+#include <boost/asio.hpp>
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
 
 #include "MessageExtractor.h"
 #include "MessageDumper.h"
+
+void DumpFromFile(const boost::filesystem::path &FN, SWatch::MessageExtractor &Parser, SWatch::MessageDumper &Dumper)
+{
+	unsigned char ReadBuff[128];
+	boost::filesystem::ifstream InS(FN, std::ios_base::binary);
+	while (InS)
+	{
+		size_t ReadCount = InS.read((char *)ReadBuff, sizeof(ReadBuff)).gcount();
+		Parser.Process(ReadBuff, ReadBuff + ReadCount, [&Dumper](bool CrcValid, const unsigned char *Begin, const unsigned char *End) {
+			Dumper(CrcValid, Begin, End);
+		});
+	}
+}
+
+void DumpFromSerialPort(const char *Name, SWatch::MessageExtractor &Parser, SWatch::MessageDumper &Dumper)
+{
+	boost::asio::io_service IOS;
+	boost::asio::serial_port Port(IOS);
+
+	constexpr size_t ReadBuffSize = 2048;
+	std::unique_ptr<unsigned char[]> ReadBuff(new unsigned char[ReadBuffSize]);
+	while (true)
+	{
+		try
+		{
+			Port.open(Name);
+			Port.set_option(boost::asio::serial_port_base::baud_rate(115200));
+			Port.set_option(boost::asio::serial_port_base::character_size(8));
+			Port.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+			Port.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port::parity::none));
+			Port.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port::flow_control::none));
+
+			while (true)
+			{
+				size_t ReadCount = Port.read_some(boost::asio::buffer(ReadBuff.get(), ReadBuffSize));
+				Parser.Process(ReadBuff.get(), ReadBuff.get() + ReadCount, [&Dumper](bool CrcValid, const unsigned char *Begin, const unsigned char *End) {
+					Dumper(CrcValid, Begin, End);
+				});
+				Dumper.flush();
+			}
+		}
+		catch (const std::exception &Ex)
+		{
+			std::cerr << "Caught exception(" << typeid(Ex).name() << "): " << Ex.what() << std::endl;
+			Port.close();
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -17,15 +68,13 @@ int main(int argc, char **argv)
 	SWatch::MessageExtractor Parser;
 	SWatch::MessageDumper Dumper(std::cout);
 
-	unsigned char ReadBuff[128];
-	std::ifstream InS(argv[1], std::ios_base::binary);
-	while (InS)
-	{
-		size_t ReadCount = InS.read((char *)ReadBuff, sizeof(ReadBuff)).gcount();
-		Parser.Process(ReadBuff, ReadBuff + ReadCount, [&Dumper](bool CrcValid, const unsigned char *Begin, const unsigned char *End) {
-			Dumper(CrcValid, Begin, End);
-		});
-	}
+	boost::filesystem::path SrcFN(argv[1]);
+	if ((boost::filesystem::exists(SrcFN)) && (boost::filesystem::status(SrcFN).type() != boost::filesystem::character_file))
+		//Treat SrcFN as a normal file.
+		DumpFromFile(SrcFN, Parser, Dumper);
+	else
+		//Treat SrcFN as a serial port.
+		DumpFromSerialPort(argv[1], Parser, Dumper);
 
 	return 0;
 }
